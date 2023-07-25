14f96bff325aa115829343bc8a9e498e28008510
ting1065 <fengtingtang1065@gmail.com>
2023-07-25 03:34:42 -0700

    set colors

---
+++ styles/colors.js
@@ -0,0 +1,9 @@
+const colors = {
+    backgroundColor: '#C6F5C9',
+    componentColor: '#05CA0F',
+    frameColor: '#61D167',
+    textColor: '#05CA0F',
+    onComponentTextColor: '#FFFFFF',
+}
+
+export default colors;
