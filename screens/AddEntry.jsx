a8787ae16f0d0834687fe984d6c0e7f4af2e0750
ting1065 <fengtingtang1065@gmail.com>
2023-07-25 19:06:25 -0700

    added buttons to adding interface

--- screens/AddEntry.jsx
+++ screens/AddEntry.jsx
@@ -1,10 +1,31 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, Alert } from "react-native";
 import React, { useState } from "react";
 import InputUnit from "../components/InputUnit";
+import PressableButton from "../components/PressableButton";
+import colors from "../styles/colors";
 
-export default function AddEntry() {
+export default function AddEntry({ navigation }) {
   const [calories, setCalories] = useState("");
   const [description, setDescription] = useState("");
+
+  function resetInput() {
+    setCalories("");
+    setDescription("");
+  }
+
+  function submit() {
+    if (
+      !calories ||
+      isNaN(calories) ||
+      parseInt(calories) <= 0 ||
+      !description
+    ) {
+      Alert.alert("Invalid input", "Please check your input values");
+      return;
+    }
+    console.log("submitted");
+    navigation.goBack();
+  }
 
   return (
     <View style={styles.container}>
@@ -20,6 +41,22 @@ export default function AddEntry() {
         desription="Description"
         inputHeight={100}
       />
+      <View style={styles.buttonContainer}>
+        <PressableButton
+          onPressFunction={() => resetInput()}
+          defaultStyle={styles.button}
+          pressedStyle={styles.buttonPressed}
+        >
+          <Text>Reset</Text>
+        </PressableButton>
+        <PressableButton
+          onPressFunction={() => submit()}
+          defaultStyle={styles.button}
+          pressedStyle={styles.buttonPressed}
+        >
+          <Text>submit</Text>
+        </PressableButton>
+      </View>
     </View>
   );
 }
@@ -28,5 +65,15 @@ const styles = StyleSheet.create({
   container: {
     paddingTop: 50,
     paddingHorizontal: 20,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+  },
+  button: {
+    backgroundColor: colors.componentColor,
+  },
+  buttonPressed: {
+    backgroundColor: colors.componentPressedColor,
   },
 });
